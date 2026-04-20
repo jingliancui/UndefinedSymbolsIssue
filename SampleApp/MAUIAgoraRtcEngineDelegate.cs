255ed255ed393abcec52da84fca13c5dc0aa06ee
jingliancui <ericcui@qq.com>
2026-04-20 22:46:45 +0800

    add code

---
+++ SampleApp/MAUIAgoraRtcEngineDelegate.cs
@@ -0,0 +1,9 @@
+using System;
+using AgoraRtcKitBinding;
+
+namespace SampleApp;
+
+public class MAUIAgoraRtcEngineDelegate:AgoraRtcEngineDelegate
+{
+
+}
---
+++ SampleApp/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace SampleApp;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+
+		Loaded+=MainPage_Loaded;
+	}
+
+	private void MainPage_Loaded(object? sender,EventArgs args)
+	{
+		var xx=AgoraRtcKitBinding.AgoraRtcEngineKit.SharedEngineWithAppId("",new MAUIAgoraRtcEngineDelegate());
+	}
+
+	private void OnCounterClicked(object? sender, EventArgs e)
+	{
+		count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
